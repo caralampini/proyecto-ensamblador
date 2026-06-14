14084fd6a8058918a2ae999659a95a74659a4e6b
Jonathan Oswaldo Martinez Esparza <anayaneitor@gmail.com>
2026-06-14 17:21:10 -0600

    ahora si ya son los parametros correctos

--- procesos.asm
+++ procesos.asm
@@ -163,55 +163,27 @@ cambiar_mapa:
     ret
 
 contar_celdas_libres:
-
-    push r12
-    push r13
-    push r14
     cmp rcx, 0
-    je .matriz_limpia
-    
-    mov r10d, 0;fila
-    mov r11d, 0;columnas
-    mov r12d, 0;contador
-    mov r13b, 0;lector
-    mov r14b, 0;desplazamiento
+    je .matriz_vacia
+    cmp edx, 0
+    je .matriz_vacia
+        
+    mov r8d, 0;contador
+    mov r9d, 0;desplazamiento
 
-    .loop_filas:
-    mov r11d, 0
-    .loop_columnas:
+    .loop_hasta_final:
+    mov r10b, [rcx+r9]
+    cmp r10b, '.'
+    jne .continuar
+    inc r8d
+    .continuar:
+    inc r9d
+    cmp r9d, edx
+    jne .loop_hasta_final
 
-    mov r14d, r10d
-    imul r14d, r8d
-    add r14d, r11d
-
-    mov  r13b, [rcx+r14d]
-    cmp r13b, '.'
-    jne .continue
-    inc r12d
-
-    .continue:
-
-    
-    inc r11d
-    cmp r11d, r8d
-    jne .loop_columnas
-
-    
-    inc r10d
-    cmp r10d, edx
-    jne .loop_filas
-
-    mov eax, r12d
-    pop r14
-    pop r13
-    pop r12
-
+    mov eax, r8d
     ret
 
-    .matriz_limpia:
-    mov eax,0
-    pop r14
-    pop r13
-    pop r12
-   
+    .matriz_vacia: 
+    mov eax, 0
     ret
